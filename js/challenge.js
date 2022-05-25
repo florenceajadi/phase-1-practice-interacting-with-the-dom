45a3d573497afb0e38842d7b562185719515a697
florence ajadi <florence.ajadi@yahoo.com>
2022-05-25 00:06:31 -0500

    commit tree 3d6f0004c028fbd13192dd786e3038557df404cf

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+const count = document.querySelector("#counter");
+const heart = document.querySelector("#likes");
+const list = document.querySelector("#list");
+const form = document.querySelector("#comment-form")
+
+
+
+  
+  counter()
+// - Manually increment and decrement the counter using the plus and minus buttons.
+// - "Like" an individual number of the counter. I should see the count of the
+//   number of "likes" associated with that number displayed.
+// - Pause the counter, which should:
+//   - pause the counter
+//   - disable all buttons except the pause button
+//   - switch the label on the button from "pause" to "resume"
+// - Click the "restart" button to restart the counter and re-enable the buttons.
+// - Leave comments on my gameplay, such as: "Wow, what a fun game this is."
